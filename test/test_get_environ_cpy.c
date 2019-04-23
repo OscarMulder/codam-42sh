14668b812577bf4326025a1adae702700c0bc85a
Oscar Mulder <omulder@student.codam.nl>
2019-04-23 14:50:00 +0200

    This is part of the previous commit

--- test/test_get_environ_cpy.c
+++ test/test_get_environ_cpy.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/19 13:43:01 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/23 14:47:18 by omulder       ########   odam.nl         */
+/*   Updated: 2019/04/23 14:49:44 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
