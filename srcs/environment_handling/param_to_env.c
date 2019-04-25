7792cc748220cb9c3ca970155c09e24513522741
Marijn Van heesewijk <mavan-he@f0r5s11.codam.nl>
2019-04-25 12:03:18 +0200

    Added malloc protection to param_to_env

--- srcs/environment_handling/param_to_env.c
+++ srcs/environment_handling/param_to_env.c
@@ -6,11 +6,25 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:45:30 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/19 20:31:36 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/04/25 12:01:47 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
+
+/*
+** add errno here
+*/
+
+char	*return_free(char **name, char **value)
+{
+	if (*name == NULL)
+		return (NULL);
+	ft_strdel(name);
+	if (*value == NULL)
+		return (NULL);
+	return (*value);
+}
 
 char	*param_to_env(char *parameter, char **vshenviron)
 {
@@ -24,13 +38,14 @@ char	*param_to_env(char *parameter, char **vshenviron)
 	while (vshenviron[env_index] != NULL)
 	{
 		name = ft_strcdup(vshenviron[env_index], '=');
+		if (name == NULL)
+			return (return_free(&name, &value));
 		if (ft_strcmp(parameter, name) == 0)
 		{
 			while (vshenviron[env_index][index] != '=')
 				index++;
 			value = ft_strdup(&(vshenviron[env_index][index + 1]));
-			ft_strdel(&name);
-			return (value);
+			return (return_free(&name, &value));
 		}
 		index = 0;
 		ft_strdel(&name);
