492825a31a2c9182362628fbc5f2bed44bfebe22
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-04-25 13:04:48 +0200

    index = i

--- srcs/environment_handling/param_to_env.c
+++ srcs/environment_handling/param_to_env.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:45:30 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/25 12:52:38 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/04/25 13:03:10 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@
 char		*param_to_env(char *parameter, char **vshenviron)
 {
 	int		env_index;
-	int		index;
+	int		i;
 	char	*name;
 	char	*value;
 
 	env_index = 0;
 	while (vshenviron[env_index] != NULL)
 	{
-		index = 0;
+		i = 0;
 		name = ft_strcdup(vshenviron[env_index], '=');
 		if (name == NULL)
 			return (NULL);
 		if (ft_strcmp(parameter, name) == 0)
 		{
-			while (vshenviron[env_index][index] != '=')
-				index++;
-			value = ft_strdup(&(vshenviron[env_index][index + 1]));
+			while (vshenviron[env_index][i] != '=')
+				i++;
+			value = ft_strdup(&(vshenviron[env_index][i + 1]));
 			ft_strdel(&name);
 			return (value);
 		}
