20954ddb3011b0cda0e6bc74f07ee9205c3e94d0
Marijn Van heesewijk <mavan-he@f0r5s11.codam.nl>
2019-04-25 12:22:16 +0200

    fixed malloc protection in param_to_env update

--- srcs/environment_handling/param_to_env.c
+++ srcs/environment_handling/param_to_env.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:45:30 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/25 12:01:47 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/04/25 12:17:06 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,15 @@
 ** add errno here
 */
 
-char	*return_free(char **name, char **value)
+static char	*return_free(char **name, char **value)
 {
-	if (*name == NULL)
-		return (NULL);
 	ft_strdel(name);
 	if (*value == NULL)
 		return (NULL);
 	return (*value);
 }
 
-char	*param_to_env(char *parameter, char **vshenviron)
+char		*param_to_env(char *parameter, char **vshenviron)
 {
 	int		env_index;
 	int		index;
@@ -39,7 +37,7 @@ char	*param_to_env(char *parameter, char **vshenviron)
 	{
 		name = ft_strcdup(vshenviron[env_index], '=');
 		if (name == NULL)
-			return (return_free(&name, &value));
+			return (NULL);
 		if (ft_strcmp(parameter, name) == 0)
 		{
 			while (vshenviron[env_index][index] != '=')
