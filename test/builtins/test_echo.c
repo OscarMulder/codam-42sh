a0e612888b47c188ca022a74b14e1100d4b1c48c
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-04-28 18:28:00 +0200

    coverage to 100

--- test/builtins/test_echo.c
+++ test/builtins/test_echo.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/28 16:14:29 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/04/28 18:20:06 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/04/28 18:27:27 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int		test_echo(void)
 
 	args[0] = "echo";
 	args[1] = "-nEa";
-	args[2] = NULL;
+	args[2] = "\n";
+	args[3] = NULL;
 	builtin_echo(args);
 	args[0] = "echo";
 	args[1] = "-nE";
@@ -27,7 +28,7 @@ int		test_echo(void)
 	builtin_echo(args);
 	args[0] = "echo";
 	args[1] = "-nEe";
-	args[2] = ft_strdup("\\\\abc\\t\\v\\r\\f");
+	args[2] = ft_strdup("\\\\abc\\t\\v\\r\\f\\n");
 	if (args[2] == NULL)
 		return (FUNCT_FAILURE);
 	args[3] = NULL;
