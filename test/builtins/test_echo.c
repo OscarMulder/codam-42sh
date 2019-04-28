11f4f6b8dfb30b2a1d81bf12a99957f4c6817656
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-04-28 17:39:50 +0200

    improved test echo

--- test/builtins/test_echo.c
+++ test/builtins/test_echo.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/28 16:14:29 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/04/28 17:17:33 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/04/28 17:38:35 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,20 @@ int		test_echo(void)
 
 	args[0] = "echo";
 	args[1] = "-neE";
-	args[2] = "\\\\abc\t\v\r\f\n";
+	args[2] = "\\\\abc\t\v\r\f";
 	args[3] = NULL;
+	builtin_echo(args);
+	args[0] = "echo";
+	args[1] = NULL;
+	builtin_echo(args);
+	args[0] = "echo";
+	args[1] = "-nE";
+	args[2] = "\n";
+	args[3] = NULL;
+	builtin_echo(args);
+	args[0] = "echo";
+	args[1] = "-nE";
+	args[2] = NULL;
 	builtin_echo(args);
 	return (FUNCT_SUCCESS);
 }
