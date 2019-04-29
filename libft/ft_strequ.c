77e069340e8c14bf773724ea548fb99afda1cdb1
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-04-29 18:18:39 +0200

    null instead of

--- libft/ft_strequ.c
+++ libft/ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/13 15:06:49 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/01/17 10:28:17 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/04/29 18:17:43 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	if (!s1 || !s2)
+	if (s1 == NULL || s2 == NULL)
 		return (-1);
 	if (ft_strcmp(s1, s2) == 0)
 		return (1);
