fe78029ba6ebe60149578fa18bcf716c16d1e821
Thijs de Jong <tde-jong@student.codam.nl>
2019-04-30 10:38:42 +0200

    norminette

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: tde-jong <tde-jong@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/10 10:10:14 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/04/30 10:29:27 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/04/30 10:38:07 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-    size_t i;
+	size_t i;
 
 	i = 0;
 	while (i < len)
@@ -23,7 +23,7 @@ char	*ft_strncpy(char *dst, const char *src, size_t len)
 			dst[i] = '\0';
 		else
 			dst[i] = src[i];
-        i++;
+		i++;
 	}
 	return (dst);
 }
