b36554fe6f1c332c3125a1ae8b0927a4d6cf16d2
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-04-30 13:40:24 +0200

    Added comments to clarify cd

--- srcs/builtins/builtin_cd.c
+++ srcs/builtins/builtin_cd.c
@@ -6,22 +6,112 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/25 17:17:25 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/04/29 16:23:43 by rkuijper      ########   odam.nl         */
+/*   Updated: 2019/04/30 12:44:43 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** WORK IN PROGRESS
 **
+** Are we going to handle -L -P -e ??
 **
-** CD changes directory
-** 1. no args:
-** change to homedir
-** 2. 1 arg
-** change dir to arg[1]
+** usage: cd cd [-L|[-P [-e]] [directory]
+**
+** - NO ARGS:
+** 	Change to HOME
+** - DISPLAY DIRECTORY:
+** 	If CDPATH is used or - is the first arg. Write path to stdout if chdir was succesful.
+** - CD -:
+** 	cd to oldpwd
+** - MORE THAN ONE ARG:
+** 	Any additional arguments following directory are ignored.
+** - SHELL VAR CDPATH:
+** 	If CDPATH exists, search for directory in CDAPTH (only if directory does not start with a /)
+** - FLAG P:
+** 	Do not follow symbolic links
+** - RETURN:
+** 	0 on success, else -1
+**
+**
+** The -P option means to not follow symbolic links:
+** symbolic links are resolved while cd is traversing directory and
+** before processing an instance of ‘..’ in directory.
+**
+** By default, or when the -L option is supplied,
+** symbolic links in directory are resolved after cd processes an instance of ‘..’ in directory.
+**
+** If ‘..’ appears in directory,
+** it is processed by removing the immediately preceding pathname component,
+** back to a slash or the beginning of directory.
+**
+** If the -e option is supplied with -P and the current working directory
+** cannot be successfully determined after a successful directory change,
+** cd will return an unsuccessful status.
+
 */
 
+
 #include "vsh.h"
+#include <sys/param.h>
+
+static int		cd_change_dir(char *path, int p)
+{
+	char *cwd;
+	char buf[MAXPATHLEN];
+
+	cwd = getcwd(buf, MAXPATHLEN);
+	// if (cwd == NULL)
+	// 	return (error_funct());
+	if (chdir(path) == 0)
+	{
+		if (p)
+			ft_putendl(path);
+		env_setkeyval("OLDPWD", cwd);
+		cwd = getcwd(buf, MAXPATHLEN);
+	// if (cwd == NULL)
+	// 	return (error_funct());
+		env_setkeyval("PWD", cwd);
+	}
+	else
+	{
+		ft_putstr_fd("cd: ", 2);
+		if (access(path, F_OK) == -1)
+			ft_putstr_fd("no such file or directory: ", 2);
+		else if (access(path, R_OK) == -1)
+			ft_putstr_fd("permission denied: ", 2);
+		else
+			ft_putstr_fd("not a directory: ", 2);
+		ft_putendl_fd(path, 2);
+		return (FUNCT_ERROR);
+	}
+	return (FUNCT_SUCCESS);
+}
+
+
+int			builtin_cd(char **args, char **env)
+{
+	int		res;
+	char	*home;
+
+	res = 0;
+	home = env_getval("HOME=");
+	if (!args[0])
+		return (cd_change_dir(home, 0));
+	res = cd_two_args(args);
+	if (res)
+		return (res - 1);
+	if (ft_strequ(args[0], "--"))
+		return (cd_change_dir(home, 0));
+	else if (args[0][0] == '-' && !args[0][2])
+		return (cd_change_dir(env_getval("OLDPWD="), 1));
+	return (cd_change_dir(args[0], 0));
+}
+
+
+
+
+
+
 
 int			builtin_cd(char **args, char **env)
 {
@@ -150,3 +240,96 @@ void	vsh_cd(char **command_row, char ***vshenviron)
 	else
 		vsh_error(E_TOOMNYARGS, command_row[0]);
 } */
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+include <stdlib.h>
+#include <unistd.h>
+#include "minishell.h"
+
+static int		cd_change_dir(char *path, int p)
+{
+	char *cwd;
+	char buf[4097];
+
+	cwd = getcwd(buf, 4096);
+	if (!chdir(path))
+	{
+		if (p)
+			ft_putendl(path);
+		env_setkeyval("OLDPWD", cwd);
+		cwd = getcwd(buf, 4096);
+		env_setkeyval("PWD", cwd);
+	}
+	else
+	{
+		ft_putstr_fd("cd: ", 2);
+		if (access(path, F_OK) == -1)
+			ft_putstr_fd("no such file or directory: ", 2);
+		else if (access(path, R_OK) == -1)
+			ft_putstr_fd("permission denied: ", 2);
+		else
+			ft_putstr_fd("not a directory: ", 2);
+		ft_putendl_fd(path, 2);
+		return (0);
+	}
+	return (1);
+}
+
+static int		cd_two_args(char **args)
+{
+	int		res;
+	char	*cwd;
+	char	*tmp;
+	char	buf[4096];
+
+	if (!args[1])
+		return (0);
+	if (args[2])
+	{
+		ft_putendl_fd("cd: too many arguments", 2);
+		return (1);
+	}
+	cwd = getcwd(buf, 4096);
+	tmp = ft_strreplace(cwd, args[0], args[1]);
+	if (!tmp)
+	{
+		ft_putstr_fd("cd: string not in pwd: ", 2);
+		ft_putendl_fd(args[0], 2);
+		free(tmp);
+		return (1);
+	}
+	res = cd_change_dir(tmp, 1);
+	free(tmp);
+	return (res + 1);
+}
+
+int				builtin_cd(char **args)
+{
+	int		res;
+	char	*home;
+
+	res = 0;
+	home = env_getval("HOME=");
+	if (!args[0])
+		return (cd_change_dir(home, 0));
+	res = cd_two_args(args);
+	if (res)
+		return (res - 1);
+	if (ft_strequ(args[0], "--"))
+		return (cd_change_dir(home, 0));
+	else if (args[0][0] == '-' && !args[0][2])
+		return (cd_change_dir(env_getval("OLDPWD="), 1));
+	return (cd_change_dir(args[0], 0));
+}
+ */
