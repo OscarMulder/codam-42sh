87cfeace843a4f67c48bdae5a5c3f4ea59cfe5ec
Rob Kuijper <rkuijper@f0r2s15.codam.nl>
2019-05-01 16:48:39 +0200

    Reworked test_env_join_key_value testcase to incorporate correct naming conventions.

--- test/environment_handling/test_env_join_key_value.c
+++ test/environment_handling/test_env_join_key_value.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   test_env_join_name_value.c                         :+:    :+:            */
+/*   test_env_join_key_value.c                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/30 16:51:29 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/05/01 15:30:59 by rkuijper      ########   odam.nl         */
+/*   Updated: 2019/05/01 16:47:48 by rkuijper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		test_env_join_key_value(void)
 {
-	if (ft_strequ(env_join_key_value("name", "value"), "name=value") != 1)
+	if (ft_strequ(env_join_key_value("key", "value"), "key=value") != 1)
 		return (FUNCT_FAILURE);
 	return (FUNCT_SUCCESS);
 }
