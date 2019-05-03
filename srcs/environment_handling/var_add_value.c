21e9b5d18d7f656a8ad29e298634545d9565682e
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-05-03 15:33:00 +0200

    initialization of var_add_value

---
+++ srcs/environment_handling/var_add_value.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   var_add_value.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/05/03 15:13:44 by mavan-he       #+#    #+#                */
+/*   Updated: 2019/05/03 15:31:56 by mavan-he      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vsh.h"
+
+int		var_add_value(char *var_key, char *var_value, char ***vararray)
+{
+
+
+	return (FUNCT_FAILURE);
+}
