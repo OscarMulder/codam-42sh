91d33a428c721c631f9f97a4d45dcd9cb717a682
Oscar Mulder <omulder@student.codam.nl>
2019-05-04 17:39:16 +0200

    General idea.

---
+++ srcs/parsing/parser_expand_wildcards.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parser_expand_wildcards.c                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: omulder <omulder@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/05/04 16:31:13 by omulder        #+#    #+#                */
+/*   Updated: 2019/05/04 17:39:05 by omulder       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vsh.h"
+
+int		parser_expand_wildcards(t_list *cmdstr_lst)
+{
+	// find wildcard
+	// find optional folder to search in
+	// find all matches 
+	// check if there is another wildcard
+	// find new matches in previous matches
+	// repeat
+	// put all matches in place of wildcard-str in lst
+}
