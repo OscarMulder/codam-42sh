c43ad9865494f84ff9d3da07d4af035ef5ca0aaf
jbrinksma <44733663+jbrinksma@users.noreply.github.com>
2019-05-05 13:00:49 +0200

    refactored ft_strndup @marijn

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/01/09 13:26:21 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/04/05 15:45:12 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/05/05 12:59:24 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,8 @@
 char	*ft_strndup(const char *s1, size_t n)
 {
 	char	*cpy;
-	size_t	i;
 
-	i = 0;
 	cpy = ft_strnew(n);
-	while (i < n && s1[i] != '\0')
-	{
-		cpy[i] = s1[i];
-		i++;
-	}
-	cpy[i] = '\0';
+	ft_strncpy(cpy, s1, n);
 	return (cpy);
 }
