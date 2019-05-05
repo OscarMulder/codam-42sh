d5a5b8df2456a1cca92a58a06e457fb3f77fa59d
jbrinksma <44733663+jbrinksma@users.noreply.github.com>
2019-05-05 12:08:25 +0200

    added explanatory comment for the parser_lexer function

--- srcs/parsing/parser_lexer.c
+++ srcs/parsing/parser_lexer.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/17 14:57:49 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/04 21:53:58 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/05/05 12:06:32 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,46 @@ void	parser_add_lst_to_lst(t_list *content_lst, t_list **lst)
 		probe->next->content = content_lst;
 	}
 }
+
+/*
+**	The goal of this function is to simple seperate the input line into
+**	a command table (cmd_tab). Firstly the commands are seperated by ';' chars,
+**	then the command lines themselves are seperated by '\t' and ' ' chars.
+**	Naturally, we are keeping track of quotes and escape chars along the way.
+**	The uninhibited(!) ';', '\'', '"', \t', and ' ' chars are removed, as are
+**	the escape chars that escaped one of those chars, since they no longer
+**	serve a special meaning in the parser after this.
+**	The resulting CMD_LIST command table is structured as follows:
+**
+**	CMD_LIST(cmd_tab)--> content(ARG_LST of command 1) -> content(char* of arg1)
+**			|								|
+**			|							next|
+**			|		(1st lst_item)		item|-----------> content(char* of arg2)
+**			|								|
+**			|							next|
+**			|							item|-----------> content(char* of arg3)
+**			|								|
+**			|							next|
+**			|							item= NULL
+**		next|
+**		item|----------> content(ARG_LST of command 2) -> content(char* of arg1)
+**			|								|
+**			|							next|
+**			|		(2nd lst_item)		item|-----------> content(char* of arg2)
+**			|								|
+**			|							next|
+**			|							item|-----------> content(char* of arg3)
+**			|								|
+**			|							next|
+**			|							item= NULL
+**		next|
+**		item|----------> content(ARG_LST of command 3) -> content(char* of arg1)
+**			|								|
+**			|							next|
+**			|		(3rd lst_item)		item|-----------> content(char* of arg2)
+**			|								|
+**		   ...							   ...
+*/
 
 int		parser_lexer(char *line, CMD_LIST **cmd_tab)
 {
