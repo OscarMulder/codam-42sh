2976aaa2edd21a41512904f12d28dce2a4fa4ce9
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-05-19 12:41:14 +0200

    scan is now added to token_lst

--- srcs/lexer/lexer_scanner.c
+++ srcs/lexer/lexer_scanner.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/19 11:12:49 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/05/19 12:16:35 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/05/19 12:39:43 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,29 @@ static void	reset_scanner(t_scanner *scanner)
 	scanner->tk_type = ERROR;
 }
 
+static int	scan_to_lst(t_list *token_lst, t_scanner *scanner)
+{
+	t_token	token;
+
+	token.type = scanner->tk_type;
+	if (token.type == WORD || token.type == ASSIGN)
+	{
+		token.value.str = ft_strndup(&(scanner->str[scanner->str_index -
+		scanner->tk_len]), scanner->tk_len);
+		if (token.value.str == NULL)
+			return (FUNCT_ERROR);
+	}
+	else if (token.type == IO_NUM)
+		token.value.io = ft_atoi(&(scanner->str[scanner->str_index]));
+	if (add_tk_to_lst(&token_lst, &token) == FUNCT_ERROR)
+		return (FUNCT_ERROR);
+	return (FUNCT_SUCCESS);
+}
+
 int			lexer_scanner(char *line, t_list *token_lst)
 {
 	t_scanner scanner;
 
-	(void)token_lst;
 	init_scanner(line, &scanner);
 	while (ft_isspace((scanner.str)[scanner.str_index]))
 		(scanner.str_index)++;
@@ -40,9 +58,11 @@ int			lexer_scanner(char *line, t_list *token_lst)
 		#ifdef DEBUG
 		print_token(&scanner);
 		#endif
+		if (scan_to_lst(token_lst, &scanner) == FUNCT_ERROR)
+			return (FUNCT_ERROR);
 		reset_scanner(&scanner);
 		while (ft_isspace((scanner.str)[scanner.str_index]))
 			(scanner.str_index)++;
 	}
-	return (1);
+	return (FUNCT_SUCCESS);
 }
