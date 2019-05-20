e3bd9a0b41b74897cbfcc0c2d2d4d26d5bab630f
Rob Kuijper <rkuijper@student.codam.nl>
2019-05-20 11:34:31 +0200

    Shortened input_parse_escape functionaltiy and removed magic numbers.

--- srcs/input_handling/input_parse_escape.c
+++ srcs/input_handling/input_parse_escape.c
@@ -6,7 +6,7 @@
 /*   By: rkuijper <rkuijper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:36:25 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/05/17 14:41:23 by rkuijper      ########   odam.nl         */
+/*   Updated: 2019/05/20 11:34:07 by rkuijper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,14 @@
 int				input_parse_escape(char c, int *input_state)
 {
 	if (*input_state == INPUT_NONE && c == '\e')
-	{
 		*input_state = INPUT_ESC;
-		return (1);
-	}
 	else if (*input_state == INPUT_ESC && c == '\e')
-	{
 		*input_state = INPUT_D_ESC;
-		return (1);
-	}
 	else if ((*input_state == INPUT_ESC || *input_state == INPUT_D_ESC) && c == '[')
-	{
 		(*input_state)++;
-		return (1);
-	}
 	else if ((*input_state == INPUT_BRACE || *input_state == INPUT_D_BRACE) && c == '3')
-	{
 		(*input_state)++;
-		return (1);
-	}
-	return (0);
+	else
+		return(FUNCT_FAILURE);
+	return (FUNCT_SUCCESS);
 }
