45d12c0fd8373b1add77c671861f5b30fbfff2c8
Oscar Mulder <omulder@student.codam.nl>
2019-05-27 17:30:36 +0200

    Replace putchar with printf

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/11 20:15:24 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/27 17:25:44 by omulder       ########   odam.nl         */
+/*   Updated: 2019/05/27 17:30:23 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	builtin_exit(int exitcode)
 {
-	ft_putchar('exit\n');
+	ft_printf("exit\n");
 	exit(exitcode);
 }
