0dbcf7d216f17b03b1cba09e409139f048d2485b
Oscar Mulder <omulder@student.codam.nl>
2019-05-28 19:09:16 +0200

    Change exitcode to unsigned char

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,13 +6,13 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/11 20:15:24 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/28 18:50:53 by omulder       ########   odam.nl         */
+/*   Updated: 2019/05/28 19:08:56 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-void	builtin_exit(char exitcode)
+void	builtin_exit(unsigned char exitcode)
 {
 	ft_printf("exit\n");
 	exit((int)exitcode);
