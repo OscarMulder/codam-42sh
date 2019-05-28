dc2d37609d79e05a5fe55aebb33b40bf98f52621
jbrinksma <44733663+jbrinksma@users.noreply.github.com>
2019-05-28 12:03:04 +0200

    adds shell_dless_input function

--- srcs/shell/shell_start.c
+++ srcs/shell/shell_start.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/18 16:44:50 by omulder        #+#    #+#                */
-/*   Updated: 2019/05/27 16:50:00 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/05/28 11:50:00 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,61 @@ void	tokenlstiter(t_tokenlst *token_lst, void (*f)(t_tokenlst *elem))
 		return ;
 	f(token_lst);
 	tokenlstiter(token_lst->next, f);
+}
+
+int		read_till_stop(char **heredoc, char *stop)
+{
+	char		c;
+	unsigned	index;
+	int			status;
+	int			input_state;
+
+	index = 0;
+	input_state = 0;
+	*heredoc = ft_strnew(0);
+	while (read(STDIN_FILENO, &c, 1) > 0)
+	{
+		if (c == '\n')
+			break ;
+		status = 0;
+		status |= input_parse_escape(c, &input_state);
+		status |= input_parse_home(c, &input_state, &index);
+		status |= input_parse_end(c, &input_state, &index, heredoc);
+		status |= input_parse_prev(c, &input_state, &index, heredoc);
+		status |= input_parse_next(c, &input_state, &index, heredoc);
+		status |= input_parse_delete(c, &input_state, &index, heredoc);
+		status |= input_parse_ctrl_up(c, &input_state, &index, heredoc);
+		status |= input_parse_ctrl_down(c, &input_state, &index, heredoc);
+		if (status == 0)
+			input_state = 0;
+		status |= input_parse_backspace(c, &index, heredoc);
+		status |= input_parse_ctrl_d(c, &index, heredoc);
+		status |= input_parse_ctrl_k(c, &index, heredoc);
+		if (status == 0 &&
+			input_parse_char(c, &index, heredoc) == FUNCT_FAILURE)
+			return (FUNCT_FAILURE);
+	}
+	return (status);
+}
+
+void	dless_input(t_tokenlst *token_lst)
+{
+	char 		*heredoc;
+	t_tokenlst	*probe;
+	char		*stop;
+
+	probe = token_lst;
+	while (probe != NULL)
+	{
+		if (probe->type == SLESS)
+		{
+			probe = probe->next;
+			stop = ft_strdup(probe->value);
+			ft_strdel(&(probe->value));
+			read_till_stop(&heredoc, stop);
+		}
+		probe = probe->next;
+	}
 }
 
 int		shell_start(void)
@@ -43,6 +98,7 @@ int		shell_start(void)
 		#ifdef DEBUG
  		tokenlstiter(token_lst, print_node);
 		#endif
+		dless_input(token_lst);
 		if (parser_start(&token_lst, &ast) != FUNCT_SUCCESS)
 			continue ;
 		#ifdef DEBUG
