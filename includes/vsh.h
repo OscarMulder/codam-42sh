d48d73580aab589213de21ce944c44d632e6599e
Thijs de Jong <tde-jong@student.codam.nl>
2019-05-31 14:59:59 +0200

    remove signal.h

--- includes/vsh.h
+++ includes/vsh.h
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:42 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/31 14:53:19 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/05/31 14:56:27 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,6 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <dirent.h>
-# include <signal.h>
 # include <stdbool.h>
 
 # include <sys/ioctl.h>
