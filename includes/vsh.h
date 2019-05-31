a3a782bfa02e01d53d106260929f35b67a3e07fa
Thijs de Jong <tde-jong@student.codam.nl>
2019-05-31 15:03:12 +0200

    remove stdlib.h

--- includes/vsh.h
+++ includes/vsh.h
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:42 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/31 15:01:11 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/05/31 15:02:49 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,6 @@
 **==================================headers=====================================
 */
 
-# include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdbool.h>
