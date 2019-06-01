3e1f6eb6b978e4c9d8036a930a40389532e02001
Jorn Brinksma <jornbrinksma@gmail.com>
2019-06-01 09:10:25 -0500

    fixes memory leak in ft_joinstrcstr

--- libft/ft_joinstrcstr.c
+++ libft/ft_joinstrcstr.c
@@ -15,12 +15,20 @@
 char		*ft_joinstrcstr(char *s1, char c, char *s2)
 {
 	char	*str;
+	char	*charstr;
 
 	str = ft_strnew(ft_strlen(s1) + 1 + ft_strlen(s2));
 	if (str == NULL)
 		return (NULL);
+	charstr = ft_chartostr(c);
+	if (charstr == NULL)
+	{
+		ft_strdel(&str);
+		return (NULL);
+	}
 	str = ft_strcpy(str, s1);
-	str = ft_strcat(str, ft_chartostr(c));
+	str = ft_strcat(str, charstr);
 	str = ft_strcat(str, s2);
+	ft_strdel(&charstr);
 	return (str);
 }
