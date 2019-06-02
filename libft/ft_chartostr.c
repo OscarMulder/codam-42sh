c07a1ddea07ebdace40322c7de45d13f0ac5afec
Jorn Brinksma <jornbrinksma@gmail.com>
2019-06-02 08:31:47 -0500

    removes unncessary byte allocated in ft_chartostr

--- libft/ft_chartostr.c
+++ libft/ft_chartostr.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/29 18:35:35 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/29 18:35:45 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/06/02 08:31:02 by root          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char		*ft_chartostr(char c)
 {
 	char	*str;
 
-	str = ft_strnew(2);
+	str = ft_strnew(1);
 	if (str == NULL)
 		return (NULL);
 	str[0] = c;
