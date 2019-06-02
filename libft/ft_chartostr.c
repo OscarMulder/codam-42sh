4b5e0cd5996f67ccf8c5987a12a604b4a2f580e9
Jorn Brinksma <44733663+jbrinksma@users.noreply.github.com>
2019-06-02 09:11:02 -0500

    fixes codam header

--- libft/ft_chartostr.c
+++ libft/ft_chartostr.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/29 18:35:35 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/06/02 08:31:02 by root          ########   odam.nl         */
+/*   Updated: 2019/06/02 08:31:02 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
