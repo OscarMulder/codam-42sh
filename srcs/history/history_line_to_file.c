4e663681e763bed7b7ad08b06aa05bdaa4392056
Oscar Mulder <32016388+OscarMulder@users.noreply.github.com>
2019-06-03 15:52:48 +0200

    Revert "History functionality (#92)"
    This reverts commit 3d0433d91d3f6a26c988c6c62c742fba32047baa.

--- srcs/history/history_line_to_file.c
+++ srcs/history/history_line_to_file.c
@@ -1,39 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   history_to_file.c                                  :+:    :+:            */
+/*   history_line_to_file.c                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/29 15:25:10 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/06/02 10:37:20 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/05/31 15:18:54 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
-#include "vsh_history.h"
-#include "libft.h"
 #include <fcntl.h>
 #include <unistd.h>
 
-int		history_to_file(void)
+int		history_line_to_file(char *line)
 {
-	int		fd;
-	int		i;
+	int	fd;
 
 	fd = open("/tmp/.vsh_history",
-		O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		O_APPEND | O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
-		ft_putstr_fd("Cannot open/create vsh history file \n", STDERR_FILENO);
+		ft_printf("Cannot open/create vsh history file \n");
 		return (FUNCT_ERROR);
 	}
-	i = 0;
-	while (history[i] != NULL)
-	{
-		ft_putendl_fd(history[i], fd);
-		i++;
-	}
+	ft_putendl_fd(line, fd);
 	close(fd);
 	return (FUNCT_SUCCESS);
 }
