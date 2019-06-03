a4c3e95ec164db6e0cd277d0a130d2bf4925bc98
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-06-03 16:59:29 +0200

    fixed malloc error

--- srcs/history/history_line_to_array.c
+++ srcs/history/history_line_to_array.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/30 18:55:25 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/06/03 15:25:56 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/06/03 16:59:17 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		history_line_to_array(char *line)
 	if (history_i < 500)
 	{
 		history[history_i] = ft_strdup(line);
-		if (history[history_i % 500] == NULL)
+		if (history[history_i] == NULL)
 			return (FUNCT_ERROR);
 	}
 	else
@@ -38,6 +38,6 @@ int		history_line_to_array(char *line)
 			return (FUNCT_ERROR);
 	}
 	history_i++;
-	history_tmp = history_i; // dit klopt niet
+	history_tmp = history_i;
 	return (FUNCT_SUCCESS);
 }
