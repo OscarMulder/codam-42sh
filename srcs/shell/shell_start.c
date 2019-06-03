d037063331fb671acc47a8a07dd7cb8e9aa8a042
Jorn Brinksma <jornbrinksma@gmail.com>
2019-06-03 05:21:46 -0500

    fixes debug error

--- srcs/shell/shell_start.c
+++ srcs/shell/shell_start.c
@@ -40,19 +40,21 @@ int		shell_start(void)
 		status = input_read(&line);
 		history_line_to_array(line);
 		#ifdef DEBUG
-		// ft_printf("\n>>>> LINE <<<<\n%s\n\n>>>> TOKEN_LST <<<<\n", line);
+		ft_printf("\n>>>> LINE <<<<\n%s\n\n>>>> TOKEN_LST <<<<\n", line);
 		#endif
 		if (lexer(&line, &token_lst) != FUNCT_SUCCESS)
 			continue ;
 		#ifdef DEBUG
- 		// lexer_tokenlstiter(token_lst, print_node);
+ 		lexer_tokenlstiter(token_lst, print_node);
 		#endif
 		shell_dless_input(token_lst);
+		#ifdef DEBUG
  		lexer_tokenlstiter(token_lst, print_node);
+		#endif
 		if (parser_start(&token_lst, &ast) != FUNCT_SUCCESS)
 			continue ;
 		#ifdef DEBUG
-		// print_tree(ast);
+		print_tree(ast);
 		#endif
 		exec_start(ast, &exit_code);
 		parser_astdel(&ast);
