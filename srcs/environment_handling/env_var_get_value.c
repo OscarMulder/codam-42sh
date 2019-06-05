f0a641c2f1064f004bf50f0ac1ae0abc5f461229
Thijs de Jong <tde-jong@student.codam.nl>
2019-06-05 09:18:32 +0200

    use strnequ instead of strncmp

--- srcs/environment_handling/env_var_get_value.c
+++ srcs/environment_handling/env_var_get_value.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:45:30 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/05/27 15:51:34 by omulder       ########   odam.nl         */
+/*   Updated: 2019/06/05 09:16:18 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char		*env_var_get_value(char *var_key, char **vararray)
 	env_i = 0;
 	while (vararray[env_i] != NULL)
 	{
-		if (ft_strncmp(var_key, vararray[env_i], var_len) == 0 &&
+		if (ft_strnequ(var_key, vararray[env_i], var_len) &&
 			vararray[env_i][var_len] == '=')
 			return (ft_strchr(vararray[env_i], '=') + 1);
 		env_i++;
