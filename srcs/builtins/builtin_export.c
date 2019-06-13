f54170d99c83c10c848ce191fff03194c5e400b4
jbrinksma <44733663+jbrinksma@users.noreply.github.com>
2019-06-13 17:30:10 +0200

    norm fix

--- srcs/builtins/builtin_export.c
+++ srcs/builtins/builtin_export.c
@@ -6,37 +6,37 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/06/05 10:33:08 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/06/07 18:00:13 by codam         ########   odam.nl         */
+/*   Updated: 2019/06/13 17:28:40 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** WORK IN PROGRESS
-** export: usage: export [-n] [name[=value] ...] or export -p
+**	WORK IN PROGRESS
+**	export: usage: export [-n] [name[=value] ...] or export -p
 **
-** TO DO:
-** NO ARGS:
+**	TO DO:
+**	NO ARGS:
 **	List var_extern, including empty keys.
-** Read in flags -n -p  (remove from var_extern to var_intern)
-** OPTION -p
-** List var_extern except if args are given
-** OPTION -n
-** remove arg keys from var_extern to var_intern (or add new key when it does not exist)
-** ARGS:
+**	Read in flags -n -p  (remove from var_extern to var_intern)
+**	OPTION -p
+**	List var_extern except if args are given
+**	OPTION -n
+**	remove arg keys from var_extern to var_intern (or add new key when it does not exist)
+**	ARGS:
 **	for each arg:
-	- If the key is followed by =value, the value of the key is set to value.
-	- check if key exists in var_intern (if yes, remove)
-	- check if key exists in var_extern (if yes, replace, return)
-	- else add key to var_extern
-
-	ERROR:
-	export =hoi
-bash: export: `=hoi': not a valid identifier
-	export -x
-bash: export: -x: invalid option
-export: usage: export [-n] [name[=value] ...] or export -p
-
-??????????? If the -f option is supplied, the names refer to shell functions; otherwise the names refer to shell variables.
+** 	- If the key is followed by =value, the value of the key is set to value.
+** 	- check if key exists in var_intern (if yes, remove)
+** 	- check if key exists in var_extern (if yes, replace, return)
+** 	- else add key to var_extern
+**
+** 	ERROR:
+** 	export =hoi
+**	bash: export: `=hoi': not a valid identifier
+**	export -x
+**	bash: export: -x: invalid option
+**	export: usage: export [-n] [name[=value] ...] or export -p
+**
+** ??????????? If the -f option is supplied, the names refer to shell functions; otherwise the names refer to shell variables.
 */
 
 #include "vsh.h"
@@ -69,7 +69,7 @@ void			builtin_export_noargs(t_envlst *envlst, int flags, int *exit_code)
 **	set exit_code to EXIT_FAILURE.
 */
 
-void			builtin_export_var(char *varname, t_envlst *envlst, int *exit_code)
+void	builtin_export_var(char *varname, t_envlst *envlst, int *exit_code)
 {
 	t_envlst	*probe;
 	int			varlen;
@@ -89,7 +89,7 @@ void			builtin_export_var(char *varname, t_envlst *envlst, int *exit_code)
 	*exit_code = EXIT_FAILURE;
 }
 
-int				builtin_export_readflags(char *arg, int *flags)
+int		builtin_export_readflags(char *arg, int *flags)
 {
 	int i;
 
@@ -112,7 +112,7 @@ int				builtin_export_readflags(char *arg, int *flags)
 	return (FUNCT_SUCCESS);
 }
 
-int				builtin_export_getflags(char **args, int *flags, int *argc)
+int		builtin_export_getflags(char **args, int *flags, int *argc)
 {
 	int				i;
 
@@ -139,7 +139,7 @@ int				builtin_export_getflags(char **args, int *flags, int *argc)
 	return (FUNCT_SUCCESS);
 }
 
-void			builtin_export(char **args, t_envlst *envlst, int *exit_code)
+void	builtin_export(char **args, t_envlst *envlst, int *exit_code)
 {
 	int	i;
 	int	flags;
