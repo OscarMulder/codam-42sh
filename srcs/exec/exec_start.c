4c35dcd6d3470eb56e8453f00872a2da7a89e93a
jbrinksma <44733663+jbrinksma@users.noreply.github.com>
2019-06-25 19:02:37 +0200

    experimenting with executor

--- srcs/exec/exec_start.c
+++ srcs/exec/exec_start.c
@@ -6,79 +6,111 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/29 17:52:22 by omulder        #+#    #+#                */
-/*   Updated: 2019/06/06 13:51:14 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/06/25 19:02:18 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-static char	**init_array(t_ast *ast)
-{
-	char	**args;
+// static char	**init_array(t_ast *ast)
+// {
+// 	char	**args;
 
-	if (ast == NULL)
-		return (NULL);
-	args = (char**)ft_memalloc(sizeof(char*) * 2);
-	if (args == NULL)
-		return (NULL);
-	args[0] = ft_strdup(ast->value);
-	if (args[0] == NULL)
-	{
-		ft_strarrdel(&args);
-		return (NULL);
-	}
-	return (args);
-}
+// 	if (ast == NULL)
+// 		return (NULL);
+// 	args = (char**)ft_memalloc(sizeof(char*) * 2);
+// 	if (args == NULL)
+// 		return (NULL);
+// 	args[0] = ft_strdup(ast->value);
+// 	if (args[0] == NULL)
+// 	{
+// 		ft_strarrdel(&args);
+// 		return (NULL);
+// 	}
+// 	return (args);
+// }
 
-static int	add_argument(char ***args, char *value)
-{
-	char	*temp;
+// static int	add_argument(char ***args, char *value)
+// {
+// 	char	*temp;
 
-	temp = ft_strdup(value);
-	if (temp == NULL)
-	{
-		ft_strarrdel(args);
-		return (FUNCT_FAILURE);
-	}
-	if (ft_strarradd(args, temp) == FUNCT_FAILURE)
-	{
-		ft_strdel(&temp);
-		ft_strarrdel(args);
-		return (FUNCT_FAILURE);
-	}
-	ft_strdel(&temp);
-	return (FUNCT_SUCCESS);
-}
+// 	temp = ft_strdup(value);
+// 	if (temp == NULL)
+// 	{
+// 		ft_strarrdel(args);
+// 		return (FUNCT_FAILURE);
+// 	}
+// 	if (ft_strarradd(args, temp) == FUNCT_FAILURE)
+// 	{
+// 		ft_strdel(&temp);
+// 		ft_strarrdel(args);
+// 		return (FUNCT_FAILURE);
+// 	}
+// 	ft_strdel(&temp);
+// 	return (FUNCT_SUCCESS);
+// }
 
-static char	**create_args(t_ast *ast)
-{
-	char	**args;
-	t_ast	*probe;
+// static char	**create_args(t_ast *ast)
+// {
+// 	char	**args;
+// 	t_ast	*probe;
 
-	args = (init_array(ast));
-	if (args == NULL)
-		return (NULL);
-	probe = ast->child;
-	while (probe)
-	{
-		if (add_argument(&args, probe->value) == FUNCT_FAILURE)
-			return (NULL);
-		probe = probe->child;
-	}
-	return (args);
-}
+// 	args = (init_array(ast));
+// 	if (args == NULL)
+// 		return (NULL);
+// 	probe = ast->child;
+// 	while (probe)
+// 	{
+// 		if (add_argument(&args, probe->value) == FUNCT_FAILURE)
+// 			return (NULL);
+// 		probe = probe->child;
+// 	}
+// 	return (args);
+// }
 
 void		exec_start(t_ast *ast, t_envlst *envlst, int *exit_code)
 {
-	char	**args;
+	// char	**args;
 
+	// if (ast->type == WORD)
+	// {
+		// args = create_args(ast);
+	// 	if (args == NULL)
+	// 		return ;
+	// 	exec_cmd(args, envlst, exit_code);
+	// }
+	t_ast *probe;
+	int i;
+
+	if (ast->sibling != NULL)
+	{
+		if (ast->type == WORD || ast->type == ASSIGN)
+			ft_printf("skipped: %s\n", ast->value);
+		else
+			ft_printf("skipped: %s\n", parser_return_token_str(ast->type));
+		exec_start(ast->sibling, envlst, exit_code);
+	}
+	???
 	if (ast->type == WORD)
 	{
-		args = create_args(ast);
-		if (args == NULL)
-			return ;
-		exec_cmd(args, envlst, exit_code);
+		probe = ast;
+		i = 0;
+		ft_printf("using: ");
+		if (ast->type == WORD || ast->type == ASSIGN)
+			ft_printf("%s -- ", ast->value);
+		else
+			ft_printf("%s -- ", parser_return_token_str(ast->type));
+		while (probe->child != NULL)
+		{
+			i++;
+			probe = probe->child;
+			if (probe->type == WORD || probe->type == ASSIGN)
+				ft_printf("%s -- ", probe->value);
+			else
+				ft_printf("%s -- ", parser_return_token_str(probe->type));
+		}
+		ft_putendl("");
 	}
-	else
-		ft_printf("N0p3: I only do the easy shit, don't fuckup!\n");
+	// else
+	// 	ft_printf("N0p3: I only do the easy shit, don't fuckup!\n");
 }
