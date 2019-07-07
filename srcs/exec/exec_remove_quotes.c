b6ee3c79f7455b82880b30daa1d0fb5efd79135d
Jorn Brinksma <jornbrinksma@gmail.com>
2019-07-07 21:39:21 +0200

    adds prototypes

---
+++ srcs/exec/exec_remove_quotes.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   exec_remove_quotes.c                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/07/07 21:35:49 by jbrinksm       #+#    #+#                */
+/*   Updated: 2019/07/07 21:39:11 by jbrinksm      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vsh.h"
+
+/*
+**	Removes the quotes that had a purpose of shell input???.
+*/
+
+void	exec_remove_quotes(t_ast *complete_command)
+{
+	
+}
