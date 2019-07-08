ac44d00d3287c39336b7c134d46868855cb17299
Jorn Brinksma <jornbrinksma@gmail.com>
2019-07-08 15:09:32 +0200

    fixes bug in tools_is_valid_identifier

--- srcs/tools/tools_is_valid_identifier.c
+++ srcs/tools/tools_is_valid_identifier.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/06/18 16:31:00 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/06/18 16:31:14 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/07/08 15:07:54 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ bool	tools_is_valid_identifier(char *str)
 {
 	int i;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL || *str == '\0' || *str == '=')
 		return (false);
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i] != '=')
 	{
 		if (ft_isalnum(str[i]) == false && str[i] != '_')
 			return (false);
