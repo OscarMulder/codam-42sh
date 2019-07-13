9a107c25b9cd7b964e3add56c38b52c83dea2da6
Jorn Brinksma <jornbrinksma@gmail.com>
2019-07-14 00:45:21 +0200

    adds unsophisticated bracketed parameter support

--- srcs/exec/exec_handle_variables.c
+++ srcs/exec/exec_handle_variables.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/07/07 20:54:47 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/07/14 00:25:01 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/07/14 00:41:26 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ static int	repl_regular_var(t_ast *item, char *val, int start, int len)
 	ft_strcat(new, &item->value[start + len]);
 	ft_strdel(&item->value);
 	item->value = new;
+	ft_printf("RESULTnb: %s\n", new);
 	return (FUNCT_SUCCESS);
 }
 
@@ -57,6 +58,52 @@ static int	handle_regular_var(t_ast *item, int *i, t_envlst *envlst)
 	return (FUNCT_SUCCESS);
 }
 
+static int	repl_bracketed_var(t_ast *item, char *val, int start, int len)
+{
+	char	*new;
+	int		val_len;
+
+	if (val != NULL)
+		val_len = ft_strlen(val);
+	else
+		val_len = 0;
+	new = ft_strnew(ft_strlen(item->value) - len + val_len);
+	if (new == NULL)
+		return (FUNCT_FAILURE);
+	ft_strncpy(new, item->value, start);
+	if (val != NULL)
+		ft_strcat(new, val);
+	ft_strcat(new, &item->value[start + len + 1]);
+	ft_strdel(&item->value);
+	item->value = new;
+	ft_printf("RESULT: %s\n", new);
+	return (FUNCT_SUCCESS);
+}
+
+static int	handle_bracketed_var(t_ast *item, int *i, t_envlst *envlst)
+{
+	int		i_offset;
+	char	*str;
+	char	*identifier;
+	char	*val;
+
+	i_offset = *i;
+	str = item->value;
+	*i += 2;
+	while (tools_isidentifierchar(str[*i]) == true)
+		(*i)++;
+	identifier = ft_strndup(&str[i_offset + 2], *i - (i_offset + 2));
+	if (identifier == NULL)
+		return (FUNCT_FAILURE);
+	val = env_getvalue(identifier, envlst);
+	if (repl_bracketed_var(item, val, i_offset, *i - i_offset) != FUNCT_SUCCESS)
+		return (FUNCT_FAILURE);
+	if (val != NULL)
+		i_offset += ft_strlen(val) - 1;
+	*i = i_offset; //is this correct?
+	return (FUNCT_SUCCESS);
+}
+
 static int	scan_var(t_ast *item, t_envlst *envlst)
 {
 	char	quote;
@@ -73,12 +120,15 @@ static int	scan_var(t_ast *item, t_envlst *envlst)
 		if (item->value[i] == '$' && quote != '\'' &&
 			tools_is_char_escaped(item->value, i) == false)
 		{
-			if (item->value[i + 1] != '{')
-				ret = handle_regular_var(item, &i, envlst);
-			// else
-			// 	ret = handle_bracketed_var(item, &i, envlst);
-			if (ret != FUNCT_SUCCESS)
-				return (FUNCT_FAILURE);
+			if (item->value[i + 1] == '{')
+				ret = handle_bracketed_var(item, &i, envlst);
+			else
+			{
+				// if (bracket_checker) ADD BRACKET CHECKER HERE
+					ret = handle_regular_var(item, &i, envlst);
+				if (ret != FUNCT_SUCCESS)
+					return (FUNCT_FAILURE);
+			}
 		}
 		i++;
 	}
