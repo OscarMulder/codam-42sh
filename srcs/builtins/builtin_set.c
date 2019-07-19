4e66d4887edf20e96a99f64fcd0dbeaac76ab17e
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-07-19 21:03:09 +0200

    clear error msg

--- srcs/builtins/builtin_set.c
+++ srcs/builtins/builtin_set.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/03 17:50:50 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/19 18:41:52 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/07/19 21:00:38 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void		builtin_set(char **args, t_envlst *envlst, int *exit_code)
 	if (args[1] != NULL)
 	{
 		ft_putendl_fd("vsh: set: too many arguments", 2);
-		ft_putendl_fd("set: usage: set [no args]", 2);
+		ft_putendl_fd("set: usage: set", 2);
 		*exit_code = FUNCT_FAILURE;
 		return ;
 	}
