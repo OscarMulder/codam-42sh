2e55a8cce7d1e01ff88541830f5aa1025c234d96
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-07-20 18:46:24 +0200

    env_sort and norm fix

--- srcs/environment_handling/env_remove_tmp.c
+++ srcs/environment_handling/env_remove_tmp.c
@@ -6,31 +6,31 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/07/18 12:14:39 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/19 20:55:27 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/07/20 18:45:13 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-static void env_node_del(t_envlst **node)
+static void	env_node_del(t_envlst **node)
 {
 	ft_strdel(&(*node)->var);
 	ft_memdel((void**)node);
 }
 
-void        env_remove_tmp(t_envlst *env)
+void		env_remove_tmp(t_envlst *env)
 {
-    t_envlst *tmp;
+	t_envlst *tmp;
 
-    if (env == NULL || env->next == NULL)
-        return ;
-    if (env->next->type == ENV_TEMP)
-    {
-        tmp = env->next;
-        env->next = env->next->next;
-        env_node_del(&tmp);
-        env_remove_tmp(env);
-    }
-    else
-        env_remove_tmp(env->next);
+	if (env == NULL || env->next == NULL)
+		return ;
+	if (env->next->type == ENV_TEMP)
+	{
+		tmp = env->next;
+		env->next = env->next->next;
+		env_node_del(&tmp);
+		env_remove_tmp(env);
+	}
+	else
+		env_remove_tmp(env->next);
 }
