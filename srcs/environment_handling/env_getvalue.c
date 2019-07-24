1c33c529377a4e49b63c7f82f1eed50217864a7d
Thijs de Jong <hi@thijsdejong.me>
2019-07-24 21:22:15 +0200

    Revert "improve env_getvalue"
    This reverts commit bb94d7005c907ffab6cb50bf850f7864f8ff402d.

--- srcs/environment_handling/env_getvalue.c
+++ srcs/environment_handling/env_getvalue.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/03 18:45:30 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/07/23 13:45:56 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/06/06 14:52:37 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,14 @@ char		*env_getvalue(char *var_key, t_envlst *envlst)
 {
 	t_envlst	*probe;
 	int			var_len;
-	char		*ret;
 
 	probe = envlst;
 	var_len = ft_strlen(var_key);
 	while (probe != NULL)
 	{
-		if (ft_strnequ(var_key, probe->var, var_len)
-			&& probe->var[var_len] == '=')
-		{
-			ret = ft_strchr(probe->var, '=') + 1;
-			if (ret == '\0')
-				return (NULL);
-			return (ret);
-		}
+		if (ft_strnequ(var_key, probe->var, var_len) &&
+			probe->var[var_len] == '=')
+			return (ft_strchr(probe->var, '=') + 1);
 		probe = probe->next;
 	}
 	return (NULL);
