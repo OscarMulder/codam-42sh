4dc967603f4c897e14de93e250429492b2f9b9ec
Oscar Mulder <omulder@student.codam.nl>
2019-07-27 16:56:06 +0200

    Norminetting history_get_file_content

--- srcs/history/history_get_file_content.c
+++ srcs/history/history_get_file_content.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/30 13:49:22 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/27 16:19:40 by omulder       ########   odam.nl         */
+/*   Updated: 2019/07/27 16:54:38 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,42 @@
 ** Get the content out of the history file
 */
 
+static int		get_line_in_history(int fd, t_vshdata *vshdata, int i)
+{
+	int		ret;
+	char	*line;
+
+	line = NULL;
+	ret = ft_get_next_line(fd, &line);
+	if (ret == -1)
+		return (FUNCT_ERROR);
+	vshdata->history[i] = (t_history*)ft_memalloc(sizeof(t_history));
+	if (vshdata->history[i] == NULL)
+		return (FUNCT_ERROR);
+	vshdata->history[i]->number = i + 1;
+	vshdata->history[i]->str = line;
+	return (ret);
+}
+
+static int		allocate_leftover_history(t_vshdata *vshdata, int i)
+{
+	while (i < HISTORY_MAX)
+	{
+		vshdata->history[i] = (t_history*)ft_memalloc(sizeof(t_history));
+		if (vshdata->history[i] == NULL)
+			return (FUNCT_ERROR);
+		i++;
+	}
+}
+
 int		history_get_file_content(t_vshdata *vshdata)
 {
 	int		fd;
 	int		ret;
 	int		i;
-	char	*line;
 
-	vshdata->history = (t_history **)ft_memalloc(sizeof(t_history *) * HISTORY_MAX);
+	vshdata->history = (t_history **)ft_memalloc(
+		sizeof(t_history *) * HISTORY_MAX);
 	if (vshdata->history == NULL || vshdata->history_file == NULL)
 		return (FUNCT_ERROR);
 	fd = open(vshdata->history_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
@@ -36,21 +64,14 @@ int		history_get_file_content(t_vshdata *vshdata)
 	i = 0;
 	while (ret > 0 && i < HISTORY_MAX)
 	{
-		line = NULL;
-		ret = ft_get_next_line(fd, &line);
-		if (ret == -1)
+		ret = get_line_in_history(fd, vshdata, i);
+		if (ret == FUNCT_ERROR)
 			return (FUNCT_ERROR);
-		vshdata->history[i] = (t_history*)ft_memalloc(sizeof(t_history)); //malloccheck
-		vshdata->history[i]->number = i + 1;
-		vshdata->history[i]->str = line;
-		i++;
-
-	}
-	while (i < HISTORY_MAX)
-	{
-		vshdata->history[i] = (t_history*)ft_memalloc(sizeof(t_history)); //malloccheck
 		i++;
 	}
 	close(fd);
+	ret = allocate_leftover_history(vshdata, i);
+	if (ret == FUNCT_ERROR)
+		return (FUNCT_ERROR);
 	return (FUNCT_SUCCESS);
 }
