241ab998bd6a069edbf008cc9591fe3046c34794
Oscar Mulder <omulder@student.codam.nl>
2019-07-28 17:09:02 +0200

    Adds malloc protec

--- srcs/history/history_change_line.c
+++ srcs/history/history_change_line.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/06/02 14:28:54 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/28 15:47:31 by omulder       ########   odam.nl         */
+/*   Updated: 2019/07/28 17:08:31 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static void	history_clear_line(unsigned *index, unsigned linelen)
 		ft_printf("\e[%dD", *index);
 }
 
-void		malloc_and_copy(t_inputdata *data, char **line, char *str)
+static int	malloc_and_copy(t_inputdata *data, char **line, char *str)
 {
 	int len;
 
@@ -41,24 +41,36 @@ void		malloc_and_copy(t_inputdata *data, char **line, char *str)
 		data->len_max *= 2;
 		ft_strdel(&(*line));
 		*line = ft_strnew(data->len_max);
+		if (*line == NULL)
+			return (FUNCT_ERROR);
 	}
 	ft_strcpy(*line, str);
+	return (FUNCT_SUCCESS);
 }
 
-void		history_change_line(t_inputdata *data, char **line, char arrow)
+static int	set_line(t_inputdata *data, char **line, int change)
+{
+	data->hist_index += change;
+	if (malloc_and_copy(data, line, data->history[data->hist_index]->str)
+	== FUNCT_ERROR)
+		return (FUNCT_ERROR);
+	return (FUNCT_SUCCESS);
+}
+
+int			history_change_line(t_inputdata *data, char **line, char arrow)
 {
 	history_clear_line(&(data->index), ft_strlen(*line));
 	if (arrow == ARROW_UP && (data->hist_index - 1) >= 0 &&
-		data->history[(data->hist_index - 1)]->str != NULL)
+	data->history[(data->hist_index - 1)]->str != NULL)
 	{
-		data->hist_index -= 1;
-		malloc_and_copy(data, line, data->history[data->hist_index]->str);
+		if (set_line(data, line, -1) == FUNCT_ERROR)
+			return (FUNCT_ERROR);
 	}
 	else if (arrow == ARROW_DOWN && (data->hist_index + 1) <= HISTORY_MAX &&
 		data->history[(data->hist_index + 1)]->str != NULL)
 	{
-		data->hist_index += 1;
-		malloc_and_copy(data, line, data->history[data->hist_index]->str);
+		if (set_line(data, line, 1) == FUNCT_ERROR)
+			return (FUNCT_ERROR);
 	}
 	else if (arrow == ARROW_DOWN)
 	{
@@ -70,4 +82,5 @@ void		history_change_line(t_inputdata *data, char **line, char arrow)
 		ft_printf("\a");
 	ft_putstr(*line);
 	data->index = ft_strlen(*line);
+	return (FUNCT_SUCCESS);
 }
