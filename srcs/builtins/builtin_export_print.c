b2775e6c2586351d834fd38da56af490e8312226
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-07-29 11:45:56 +0200

    set and export print escape chars

--- srcs/builtins/builtin_export_print.c
+++ srcs/builtins/builtin_export_print.c
@@ -6,16 +6,44 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/07/19 12:13:35 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/21 18:10:42 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/07/29 11:22:31 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
+static bool	escape_char(char c)
+{
+	return (c == '$' || c == '\"' || c == '\\');
+}
+
+static void	builtin_export_escape_print(char *var)
+{
+	char		*value;
+	int			i;
+
+	value = ft_strchr(var, '=');
+	if (value != NULL)
+	{
+		i = 1;
+		ft_printf("%.*s\"", value - var + 1, var);
+		while (value[i] != '\0')
+		{
+			if (escape_char(value[i]) == true)
+				ft_putchar('\\');
+			ft_putchar(value[i]);
+			i++;
+		}
+		ft_putstr("\"\n");
+	}
+	else
+		ft_putendl(var);
+
+}
+
 void		builtin_export_print(t_envlst *envlst, int flags)
 {
 	t_envlst	*probe;
-	char		*value;
 
 	probe = envlst;
 	while (probe != NULL)
@@ -24,12 +52,7 @@ void		builtin_export_print(t_envlst *envlst, int flags)
 		{
 			if (flags & EXP_FLAG_LP)
 				ft_putstr("declare -x ");
-			value = ft_strchr(probe->var, '=');
-			if (value != NULL)
-				ft_printf("%.*s\"%s\"\n", value - probe->var + 1,
-				probe->var, value + 1);
-			else
-				ft_putendl(probe->var);
+			builtin_export_escape_print(probe->var);
 		}
 		probe = probe->next;
 	}
--- srcs/tools/tool_check_for_special.c
+++ srcs/tools/tool_check_for_special.c
@@ -6,17 +6,17 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/07/21 17:54:17 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/07/21 17:54:54 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/07/29 11:35:38 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-bool	tool_check_for_whitespace(char *str)
+bool	tool_check_for_special(char *str)
 {
 	while (*str != '\0')
 	{
-		if (ft_isspace(*str) == true)
+		if (tool_is_special(*str) == true)
 			return (true);
 		str++;
 	}
