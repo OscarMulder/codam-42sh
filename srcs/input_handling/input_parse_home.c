152df0507ea7e382df721ae793606ff06c6f2e7d
Oscar Mulder <omulder@student.codam.nl>
2019-07-31 13:08:48 +0200

    Code cleanup

--- srcs/input_handling/input_parse_home.c
+++ srcs/input_handling/input_parse_home.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:37:33 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/07/31 12:33:42 by omulder       ########   odam.nl         */
+/*   Updated: 2019/07/31 13:08:24 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,11 @@
 
 int	input_parse_home(t_inputdata *data)
 {
-	if (data->index > 0 &&
-	((data->input_state == INPUT_BRACE && data->c == 'H') || data->c == '\1'))
+	if (data->input_state == INPUT_BRACE && data->c == 'H') || data->c == '\1')
 	{
-		ft_printf("\e[%dD", data->index);
+		if (data->index > 0)
+			ft_printf("\e[%dD", data->index);
 		data->index = 0;
-		data->input_state = INPUT_NONE;
-		return (FUNCT_SUCCESS);
-	}
-	else if (data->index == 0 &&
-	((data->input_state == INPUT_BRACE && data->c == 'H') || data->c == '\1'))
-	{
 		data->input_state = INPUT_NONE;
 		return (FUNCT_SUCCESS);
 	}
