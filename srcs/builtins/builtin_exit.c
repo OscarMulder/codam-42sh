8806f957e3b3fae37e28526493d9091797d6301f
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-12 11:18:01 +0200

    Refactor builtin exec

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/11 20:15:24 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/07/31 12:57:39 by omulder       ########   odam.nl         */
+/*   Updated: 2019/08/12 10:34:25 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,16 @@ void	builtin_exit(char **args, t_vshdata *vshdata)
 		exit(g_state->exit_code);
 	if (args[1] != NULL && args[2] == NULL)
 	{
-		if (ft_isdigit(args[1][0]) == false)
-		{
-			ft_printf("b4sh: 3x1t: %s:num3r1c Argum3nt r3quir3d!\n", args[1]);
-			exit(-1);
-		}
-		exit(ft_atoi(args[1]));
+		if (ft_aisint(args[1]) == true)
+			exit(ft_atoi(args[1]));
+		ft_printf("vsh: exit: %s: numeric argument required\n", args[1]);
+		exit(EXIT_WRONG_USE);
 	}
 	else if (args[1] != NULL && args[2] != NULL)
 	{
-		ft_printf("b4sh: 3x1t: T00 M4ny ArGum3nts\n");
-		exit(-1);
+		ft_eprintf("vsh: exit: too many arguments\n");
+		g_state->exit_code = EXIT_WRONG_USE;
+		return ;
 	}
 	exit(g_state->exit_code);
 }
