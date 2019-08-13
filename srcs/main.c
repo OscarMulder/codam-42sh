5952e81d6c46df508779675bb14474e1b967be84
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-13 14:51:56 +0200

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:49 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/08/13 14:39:32 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/08/13 14:46:09 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,13 @@ int		main(int argc, char **argv)
 	(void)argv;
 	(void)argc;
 	g_state = (t_state*)ft_memalloc(sizeof(t_state));
+	if (g_state == NULL)
+		return (EXIT_FAILURE);
 	g_state->exit_code = EXIT_SUCCESS;
 	if (shell_init_vshdata(&vshdata) == FUNCT_ERROR)
-		return (EXIT_FAILURE);
-	term_p = term_prepare(vshdata.envlst);
-	if (g_state == NULL || term_p == NULL)
 		return (EXIT_FAILURE);
 	if (redir_save_stdfds(&vshdata) == FUNCT_ERROR)
 		return (EXIT_FAILURE);
 	shell_start(&vshdata);
-	if (term_reset(term_p) == FUNCT_FAILURE)
-		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
