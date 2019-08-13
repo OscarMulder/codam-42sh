3384027ec1ffa1c0b0e36f75aa9459e332656491
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-13 15:23:10 +0200

    Reset terminal settings when using builtin exit

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,24 +6,31 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/11 20:15:24 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/08/12 10:34:25 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/08/13 15:22:25 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-void	builtin_exit(char **args, t_vshdata *vshdata)
+static void	reset_exit(int exit_code, t_term *term_p)
+{
+	if (term_reset(term_p) == FUNCT_FAILURE)
+		ft_eprintf("vsh: could not reset terminal settings\n");
+	exit(exit_code);
+}
+
+void		builtin_exit(char **args, t_vshdata *vshdata)
 {
 	ft_printf("exit\n");
 	history_to_file(vshdata);
 	if (args == NULL)
-		exit(g_state->exit_code);
+		reset_exit(g_state->exit_code, vshdata->term);
 	if (args[1] != NULL && args[2] == NULL)
 	{
 		if (ft_aisint(args[1]) == true)
 			exit(ft_atoi(args[1]));
 		ft_printf("vsh: exit: %s: numeric argument required\n", args[1]);
-		exit(EXIT_WRONG_USE);
+		reset_exit(EXIT_WRONG_USE, vshdata->term);
 	}
 	else if (args[1] != NULL && args[2] != NULL)
 	{
@@ -31,5 +38,5 @@ void	builtin_exit(char **args, t_vshdata *vshdata)
 		g_state->exit_code = EXIT_WRONG_USE;
 		return ;
 	}
-	exit(g_state->exit_code);
+	reset_exit(g_state->exit_code, vshdata->term);
 }
