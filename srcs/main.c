f7992efebaefc1eead154c5a9d95b3bf13ea5fa7
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-13 15:03:59 +0200

    remove unused argv, argc

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:49 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/08/13 15:03:27 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/08/13 15:03:47 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@
 **	ft_printf alloc error handling
 */
 
-int		main(int argc, char **argv)
+int		main(void)
 {
 	t_vshdata	vshdata;
 
-	(void)argv;
-	(void)argc;
 	g_state = (t_state*)ft_memalloc(sizeof(t_state));
 	if (g_state == NULL)
 		return (EXIT_FAILURE);
