2154b6eff0c4a03aa716ac4468442d92d6c4af57
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-08-14 15:48:19 +0200

    auto_big_lst

--- srcs/autocomplete/auto_big_lst.c
+++ srcs/autocomplete/auto_big_lst.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/13 20:07:40 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/08/13 20:11:39 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/08/14 15:47:10 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 
 int		auto_big_lst(t_list *matchlst, int lst_len)
 {
-	(void)matchlst;
+	char c;
+
 	ft_printf("Display all %d possibilities? (y or n)", lst_len);
+	c = 0;
+	while (c != 'y' && c != 'n')
+	{
+		if (read(STDIN_FILENO, &c, 1) == -1)
+		{
+			ft_eprintf("vsh: error reading input\n");
+			return (FUNCT_ERROR);
+		}
+	}
+	ft_putchar('\n');
+	if (c == 'y')
+		auto_lst_print(matchlst);
+	else if (c == 'n')
+		return (FUNCT_FAILURE);
 	return (FUNCT_SUCCESS);
 }
