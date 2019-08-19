72940a17911594f4298e7f4d13da07462dda7aed
Oscar Mulder <omulder@student.codam.nl>
2019-08-19 14:43:05 +0200

    Turn off debug

--- includes/vsh.h
+++ includes/vsh.h
@@ -6,13 +6,13 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:42 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/08/19 12:18:12 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/08/19 14:16:56 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VSH_H
 # define VSH_H
-# define DEBUG
+// # define DEBUG
 # include <sys/stat.h>
 # include <fcntl.h>
 
