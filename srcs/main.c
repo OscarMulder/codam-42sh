cf4ab3f68a72372d6e73f30910ee174c5652c3b1
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-22 15:26:16 +0200

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:49 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/08/13 15:03:47 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/08/22 15:26:05 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ int		main(void)
 {
 	t_vshdata	vshdata;
 
+	if (isatty(STDIN_FILENO) != 1)
+	{
+		ft_eprintf("STDIN is not an open file descriptor referring to a "
+					"terminal\n");
+		return (EXIT_FAILURE);
+	}
 	g_state = (t_state*)ft_memalloc(sizeof(t_state));
 	if (g_state == NULL)
 		return (EXIT_FAILURE);
