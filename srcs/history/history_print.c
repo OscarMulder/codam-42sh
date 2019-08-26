89f41d2f6f50041092d7dfd5195622290fca01d8
Oscar Mulder <omulder@student.codam.nl>
2019-08-26 14:39:51 +0200

    Add tabs on multiline history print

--- srcs/history/history_print.c
+++ srcs/history/history_print.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/30 20:47:41 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/08/02 09:49:13 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/08/24 14:33:46 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,49 @@
 /*
 ** Print the history
 */
+
+static char	*add_tabs(char *str, char *new)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		new[j] = str[i];
+		if (str[i] == '\n' && str[i + 1] != '\0')
+		{
+			j++;
+			new[j] = '\t';
+		}
+		i++;
+		j++;
+	}
+	return (new);
+}
+
+static char	*add_tabs_after_newlines(char *str)
+{
+	int		i;
+	int		count;
+	char	*new;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n' && str[i + 1] != '\0')
+			count++;
+		i++;
+	}
+	if (count == 0)
+		return (ft_strdup(str));
+	new = ft_strnew(i + count);
+	if (new == NULL)
+		return (NULL);
+	return (add_tabs(str, new));
+}
 
 static void	find_start(t_history **history, int *smallest, int *start)
 {
@@ -36,21 +79,26 @@ static void	find_start(t_history **history, int *smallest, int *start)
 
 void		history_print(t_history **history)
 {
-	int i;
-	int	smallest;
-	int start;
+	int		i;
+	int		smallest;
+	int		start;
+	char	*tmp;
 
 	find_start(history, &smallest, &start);
 	i = start;
 	while (i < HISTORY_MAX && history[i]->str != NULL)
 	{
-		ft_printf("%5d  %s\n", history[i]->number, history[i]->str);
+		tmp = add_tabs_after_newlines(history[i]->str);
+		ft_printf("%d\t%s\n", history[i]->number, tmp);
+		ft_strdel(&tmp);
 		i++;
 	}
 	i = 0;
 	while (start != 0 && i < start && history[i]->str != NULL)
 	{
-		ft_printf("%5d  %s\n", history[i]->number, history[i]->str);
+		tmp = add_tabs_after_newlines(history[i]->str);
+		ft_printf("%d\t%s\n", history[i]->number, history[i]->str);
+		ft_strdel(&tmp);
 		i++;
 	}
 }
