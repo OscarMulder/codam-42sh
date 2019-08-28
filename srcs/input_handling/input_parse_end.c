5c7aa683c862cf32d39dc9c0349f50ca6f8f0520
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-28 16:53:04 +0200

    Update input_parse_end.c

--- srcs/input_handling/input_parse_end.c
+++ srcs/input_handling/input_parse_end.c
@@ -6,7 +6,7 @@
 /*   By: rkuijper <rkuijper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:39:04 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/08/27 12:24:44 by omulder       ########   odam.nl         */
+/*   Updated: 2019/08/28 16:52:04 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int			curs_go_end(t_vshdata *data)
 	size_t	index_to_end;
 
 	index_to_end = data->line->len_cur - data->line->index;
-	if (data->line->len_cur > data->line->index)
+	if (index_to_end > 0)
 		curs_move_n_right(data, index_to_end);
 	return (FUNCT_SUCCESS);
 }
