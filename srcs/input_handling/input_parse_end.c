5c511d49a62aacb5f9795ac78fcb4b31d0a316d4
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-28 16:58:50 +0200

    Update input_parse_end.c

--- srcs/input_handling/input_parse_end.c
+++ srcs/input_handling/input_parse_end.c
@@ -6,19 +6,19 @@
 /*   By: rkuijper <rkuijper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:39:04 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/08/28 16:52:04 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/08/28 16:54:28 by tde-jong      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 #include <sys/ioctl.h>
 
+/*
+**	Calculates steps to end and then uses curs_move_n_right.
+*/
+
 int			curs_go_end(t_vshdata *data)
 {
-	size_t	index_to_end;
-
-	index_to_end = data->line->len_cur - data->line->index;
-	if (index_to_end > 0)
-		curs_move_n_right(data, index_to_end);
+	curs_move_n_right(data, data->line->len_cur - data->line->index);
 	return (FUNCT_SUCCESS);
 }
