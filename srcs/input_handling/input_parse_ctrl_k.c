a7193b077b9271c960ed16ba88b256ed87eccc81
Jorn Brinksma <jornbrinksma@gmail.com>
2019-08-29 14:58:00 +0200

    change affected lines from 20 to 1, not sure why it was 20

--- srcs/input_handling/input_parse_ctrl_k.c
+++ srcs/input_handling/input_parse_ctrl_k.c
@@ -6,7 +6,7 @@
 /*   By: rkuijper <rkuijper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:48:04 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/08/29 12:03:37 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/08/29 14:25:21 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void		input_parse_ctrl_k(t_vshdata *data)
 		ft_bzero(&data->line->line[data->line->index],
 			data->line->len_cur - data->line->index);
 		curs_go_home(data);
-		tputs(data->termcaps->tc_clear_lines_str, 20, &ft_tputchar); // why 20?
+		tputs(data->termcaps->tc_clear_lines_str, 1, &ft_tputchar);
 		data->line->len_cur = ft_strlen(data->line->line);
 		data->line->index = data->line->len_cur;
 		input_print_str(data, data->line->line);
--- srcs/input_handling/input_parse_ctrl_u.c
+++ srcs/input_handling/input_parse_ctrl_u.c
@@ -6,7 +6,7 @@
 /*   By: rkuijper <rkuijper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/08/23 12:15:47 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/08/29 12:03:55 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/08/29 14:20:13 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void		input_parse_ctrl_u(t_vshdata *data)
 		ft_bzero(&data->line->line[data->line->index],
 			data->line->len_cur - data->line->index);
 		curs_go_home(data);
-		tputs(data->termcaps->tc_clear_lines_str, 20, &ft_tputchar); // why 20?	
+		tputs(data->termcaps->tc_clear_lines_str, 1, &ft_tputchar);
 		data->line->len_cur = ft_strlen(data->line->line);
 		data->line->index = data->line->len_cur;
 		input_print_str(data, data->line->line);
