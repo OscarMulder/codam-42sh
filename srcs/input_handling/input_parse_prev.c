d61adf7dc0588bc1e5100ae23cf801169c4398ad
Thijs de Jong <tde-jong@student.codam.nl>
2019-08-29 15:49:42 +0200

    Update input_parse_prev.c

--- srcs/input_handling/input_parse_prev.c
+++ srcs/input_handling/input_parse_prev.c
@@ -26,27 +26,25 @@ void		curs_move_prev_word(t_vshdata *data)
 		return ;
 	i = 0;
 	if ((data->line->index > 0
-		&& tools_isprintnotblank(data->line->line[data->line->index]) == true // i++ if at beginning of previous word
+		&& tools_isprintnotblank(data->line->line[data->line->index]) == true
 		&& ft_isblank(data->line->line[data->line->index - 1]) == true)
-		|| (data->line->index > 0 && data->line->index == data->line->len_cur)) // i++ if at end of line
+		|| (data->line->index > 0 && data->line->index == data->line->len_cur))
 		i++;
-	while (data->line->index - i > 0 // i++ blanks
+	while (data->line->index - i > 0
 		&& ft_isblank(data->line->line[data->line->index - i]) == true)
 		i++;
 	if (data->line->index - i == 0)
 		curs_move_n_left(data, i);
 	else
 	{
-		while (data->line->index - i > 0
-			&& tools_isprintnotblank(data->line->line[data->line->index - i - 1]))
+		while (data->line->index - i > 0 && tools_isprintnotblank(
+				data->line->line[data->line->index - i - 1]))
 			i++;
 		curs_move_n_left(data, i);
 	}
 }
 
 /*
-**	`ws` will be taken from `data` after Oscar is done.
-**
 **	Calculations to move n times to the left (or up if necessar) on the current
 **	ws. If used after some weird screen clearing, make sure to compensate
 **	for the automatic `index` change if necessar
@@ -54,8 +52,11 @@ void		curs_move_prev_word(t_vshdata *data)
 
 static void	move_left_parse_newline(t_vshdata *data)
 {
-	char *pos = ft_strrnchr(data->line->line, '\n', data->line->index);
-	int len = data->line->index;
+	char	*pos;
+	int		len;
+
+	pos = ft_strrnchr(data->line->line, '\n', data->line->index);
+	len = data->line->index;
 	if (pos != NULL)
 		len = (data->line->index - 1) - (pos - data->line->line);
 	ft_putstr("\e[A");
@@ -103,14 +104,9 @@ void		curs_move_n_left(t_vshdata *data, size_t n)
 		else
 			move_left_to_colmax(data, ws.ws_col);
 	}
-	#ifdef DEBUG
-	ft_eprintf("New cursor coordinates: [%d:%d]\n", data->curs->coords.x, data->curs->coords.y);
-	#endif
 }
 
 /*
-**	`ws` will be taken from data after Oscar is done.
-**
 **	Moves the cursor (and index) one to the left (or up if necessary)
 **	If used after some weird screen clearing, make sure to compensate
 **	for the automatic `index` change if necessar
