fa4237ed8c2f8c7aa9b89723df7f279b660210ec
Rob Kuijper <rkuijper@student.codam.nl>
2019-09-09 14:32:37 +0200

    Added builtin_jobs.c file.

---
+++ srcs/builtins/builtin_jobs.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   input_read.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: omulder <omulder@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/04/17 14:03:16 by rkuijper       #+#    #+#                */
+/*   Updated: 2019/09/02 13:58:55 by rkuijper      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <vsh.h>
+
+/*
+**	Parse options -p and -l
+**	There's also the 'job' parameter which can be present in args, but it doesn't work(?)
+**
+*/
+
+void		builtin_jobs(char **args, t_vshdata *data)
+{
+
+}
