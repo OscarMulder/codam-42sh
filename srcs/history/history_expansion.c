31fb82644c36079de823e856777b6228b20bfe33
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-09-11 17:45:10 +0200

    history_get_line_at_index

--- srcs/history/history_expansion.c
+++ srcs/history/history_expansion.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/10 12:37:29 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/09/10 18:39:32 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/09/11 17:43:32 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int		history_expand(t_datahistory *history, char **line, size_t *i)
 		return (FUNCT_SUCCESS);
 	history_line = history_get_line(history, *line, *i);
 	if (history_line == NULL)
-		return (FUNCT_ERROR);
+		return (FUNCT_FAILURE); // print error
 	// replace !x with history_line and set i to end of expan
 	return (FUNCT_SUCCESS);
 }
--- srcs/history/history_get_line.c
+++ srcs/history/history_get_line.c
@@ -6,19 +6,42 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/10 18:35:45 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/09/10 18:42:18 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/09/11 17:43:54 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
-static char	*history_get_line_at_index(t_datahistory *history, int i)
+/*
+**	History_get_index figures out at which index we need to look for a str
+**	It verifies if it is within the valid range of min and max history nums
+**	It sets his_index to a valid index in history or it returns failure 
+*/
+
+int			history_get_index(int *his_index, int his_total, int i)
 {
-	(void)history;
-	(void)i;
-	return (NULL);
+	if (i < 0)
+		i = his_total + 1 + i;
+	if (i <= 0)
+		return (FUNCT_FAILURE);
+	if (i > his_total || i <= his_total - HISTORY_MAX)
+		return (FUNCT_FAILURE);
+	*his_index = (i - 1) % HISTORY_MAX;
+	return (FUNCT_SUCCESS);
 }
 
+static char	*history_get_line_at_index(t_datahistory *history, int i)
+{
+	int his_index;
+
+	his_index = 0;
+	if (history_get_index(&his_index,
+		history->history[history->hist_start]->number, i) == FUNCT_FAILURE)
+		return (NULL);
+	if (history->history[his_index] == NULL)
+		return (NULL);
+	return (history->history[his_index]->str);
+}
 
 char		*history_get_line(t_datahistory *history, char *line, size_t i)
 {
