2b871918282c8903f88d6da4b12c62c420cc2197
Oscar Mulder <omulder@student.codam.nl>
2019-09-12 15:16:32 +0200

    Fix fc -l all functionality

--- srcs/builtins/builtin_fc/builtin_fc_list.c
+++ srcs/builtins/builtin_fc/builtin_fc_list.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/11 12:54:36 by omulder        #+#    #+#                */
-/*   Updated: 2019/09/12 13:03:33 by omulder       ########   odam.nl         */
+/*   Updated: 2019/09/12 15:13:42 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,14 +94,101 @@ static void	print_reverse(int start, int end, t_history **history, t_fcdata *fc)
 		*end = start - 1;
 } */
 
-int			find_first(t_datahistory *history, t_fcdata *fc, int *start)
+int			find_history_item_str(t_datahistory *history, char *match, int *index)
 {
+	int i;
 
+	i = history->hist_start;
+	while (i >= 0 && history->history[i]->str != NULL)
+	{
+		if (ft_strncmp(history->history[i]->str, match, ft_strlen(match)) == 0)
+		{
+			*index = i;
+			return (FUNCT_SUCCESS);
+		}
+		i--;
+	}
+	if (i == 0)
+		i = (HISTORY_MAX - 1);
+	while (i > history->hist_start && history->history[i]->str != NULL)
+	{
+		if (ft_strncmp(history->history[i]->str, match, ft_strlen(match)) == 0)
+		{
+			*index = i;
+			return (FUNCT_SUCCESS);
+		}
+		i--;
+	}
+	return (FUNCT_FAILURE);
 }
 
-int			find_last(t_datahistory *history, t_fcdata *fc, int *end)
+int			find_hist_num_backup(t_datahistory *history, int num, int *index)
 {
-	
+	int i;
+
+	i = history->hist_start;
+	while (i >= 0 && history->history[i]->str != NULL)
+	{
+		if (history->history[i]->number == num)
+		{
+			*index = i;
+			return (FUNCT_SUCCESS);
+		}
+		i--;
+	}
+	if (i == 0)
+		i = (HISTORY_MAX - 1);
+	while (i >= history->hist_start && history->history[i]->str != NULL)
+	{
+		if (history->history[i]->number == num)
+		{
+			*index = i;
+			return (FUNCT_SUCCESS);
+		}
+		i--;
+	}
+	return (FUNCT_FAILURE);
+}
+
+int			find_history_item_num(t_datahistory *history, int num, int *index)
+{
+	int	his_max_num;
+
+	his_max_num = history->history[history->hist_start]->number;
+	if (num < 0)
+		num = his_max_num + 1 + num;
+	if (num == 0)
+		return (FUNCT_FAILURE);
+	if (num > his_max_num || num <= his_max_num - HISTORY_MAX)
+		return (FUNCT_FAILURE);
+	else if (history->history[(num - 1) % HISTORY_MAX]->str != NULL &&
+	history->history[(num - 1) % HISTORY_MAX]->number == num)
+	{
+		*index = (num - 1) % HISTORY_MAX;
+		return (FUNCT_SUCCESS);
+	}
+	return (find_hist_num_backup(history, num, index));
+}
+
+int			find_index(t_datahistory *history, char *str, int *index)
+{
+	if (ft_isdigit(str[0]) || str[0] == '+' || str[0] == '-')
+	{
+		if (find_history_item_num(history, ft_atoi(str), index) == FUNCT_FAILURE)
+		{
+			ft_eprintf("ERROR4\n");
+			return (FUNCT_FAILURE);
+		}
+	}
+	else
+	{
+		if (find_history_item_str(history, str, index) == FUNCT_FAILURE)
+		{
+			ft_eprintf("ERROR3\n");
+			return (FUNCT_FAILURE);
+		}
+	}
+	return (FUNCT_SUCCESS);
 }
 
 void		fc_list(t_datahistory *history, t_fcdata *fc)
@@ -111,7 +198,7 @@ void		fc_list(t_datahistory *history, t_fcdata *fc)
 
 	if (fc->first == NULL)
 	{
-		end = history->hist_start + 1;
+		end = history->hist_start;
 		start = end - 15;
 		if (start < 0 && history->history[0]->number != 1)
 			start = HISTORY_MAX - start;
@@ -120,16 +207,16 @@ void		fc_list(t_datahistory *history, t_fcdata *fc)
 	}
 	else
 	{
-		if (find_first(history, fc, &start) == FUNCT_ERROR)
+		if (find_index(history, fc->first, &start) == FUNCT_ERROR)
 		{
 			ft_eprintf("ERROR1\n");
 			return ;
 		}
 		if (fc->last == NULL)
-			end = history->hist_start + 1;
+			end = history->hist_start;
 		else
 		{
-			if (find_last(history, fc, &end) == FUNCT_ERROR)
+			if (find_index(history, fc->last, &end) == FUNCT_ERROR)
 			{
 				ft_eprintf("ERROR2\n");
 				return ;
