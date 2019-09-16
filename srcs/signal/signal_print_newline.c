8266b71d1853c162589c0b9c1a44fc64adabaa0a
Jorn Brinksma <jornbrinksma@gmail.com>
2019-09-16 11:39:51 +0200

    fixes bug in and clarifies signal_print_newline

--- srcs/signal/signal_print_newline.c
+++ srcs/signal/signal_print_newline.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/16 08:14:45 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/09/16 10:10:01 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/09/16 11:38:49 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void		signal_print_newline(int signum)
 {
-	(void)signum;
-	ft_putchar('%');
-	ft_putchar('\n');
-	signal(SIGINT, signal_print_newline);
+	if (signum == SIGINT)
+	{
+		ft_putchar('\n');
+		signal(SIGINT, signal_print_newline);
+	}
 }
