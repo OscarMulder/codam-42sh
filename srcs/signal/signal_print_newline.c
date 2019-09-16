eab770170888b9591954044ff1598825f7dc58c8
Jorn Brinksma <jornbrinksma@gmail.com>
2019-09-16 17:13:27 +0200

    add missing header

--- srcs/signal/signal_print_newline.c
+++ srcs/signal/signal_print_newline.c
@@ -6,11 +6,12 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/16 08:14:45 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/09/16 11:38:49 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/09/16 17:12:06 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
+#include <signal.h>
 
 void		signal_print_newline(int signum)
 {
