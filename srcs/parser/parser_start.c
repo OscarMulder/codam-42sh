6de59eceba2167bd46c9d4e7fa5f7b04cfcc73ac
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-09-23 15:31:11 +0200

    fixed issue with ; in linecon ctrl d

--- srcs/parser/parser_start.c
+++ srcs/parser/parser_start.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/19 19:58:40 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/09/16 12:40:30 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/09/23 15:29:45 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ static bool	parser_list(t_tokenlst **token_lst, t_ast **ast)
 		{
 			if (parser_add_astnode(token_lst, ast) == false)
 				return (parser_return_del(ast));
-			if (TK_TYPE != NEWLINE)
+			if (TK_TYPE != NEWLINE && TK_TYPE != END)
 			{
 				if (parser_add_right(token_lst, ast, &parser_list) == false)
 					return (false);
