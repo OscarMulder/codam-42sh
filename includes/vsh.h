938ecaee9eb56c1602dd215edfa331d884b273dc
Jorn Brinksma <jornbrinksma@gmail.com>
2019-10-05 16:38:36 +0200

    fix norm error

--- includes/vsh.h
+++ includes/vsh.h
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/10 20:29:42 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/10/01 15:36:26 by omulder       ########   odam.nl         */
+/*   Updated: 2019/10/05 16:37:47 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -676,8 +676,8 @@ void			input_parse_ctrl_k(t_vshdata *data);
 void			input_parse_ctrl_u(t_vshdata *data);
 void			input_parse_ctrl_y(t_vshdata *data);
 void			input_parse_tab(t_vshdata *data);
-int				get_curs_row();
-void			input_reset_cursor_pos();
+int				get_curs_row(void);
+void			input_reset_cursor_pos(void);
 void			resize_window_check(int sig);
 int				input_add_chunk(t_vshdata *data, char *chunk,
 				int chunk_len);
