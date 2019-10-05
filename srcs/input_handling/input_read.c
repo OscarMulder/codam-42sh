015dec032cfc0b3de9b27c63e96ccc5a31a1a7fb
Jorn Brinksma <jornbrinksma@gmail.com>
2019-10-05 15:43:01 +0200

    Add comment to input_read and input_parse

--- srcs/input_handling/input_read.c
+++ srcs/input_handling/input_read.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/17 14:03:16 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/10/01 12:14:36 by tde-jong      ########   odam.nl         */
+/*   Updated: 2019/10/05 15:41:33 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,37 @@ static int	reset_input_read_return(t_vshdata *data, int ret)
 	return (ret);
 }
 
+/*
+**	First we check for the `signals`, of our input. If it is not one of those,
+**	we check if it is a '\e' character, which might indicate the start of a
+**	recognised ANSI escape sequence. If that is not the case, we will see
+**	if the character indicates something special like cursor movement. And
+**	Finally if that is not the case either, we will attempt to add the
+**	byte into our command line.
+**
+**	@return NEW_PROMPT
+**					This will be returned when a ctrl+c character is read
+**					('\3'). It lets the shell know that the current line
+**					is cancelled.
+**
+**	@return IR_EOF	This stands for "Input Read End Of File". It will occur
+**					when we read a ctrl+d ('\4') character, and the current
+**					line is empty, and the current prompt is not our main
+**					prompt (think of a `dquote >` for example). The reason
+**					IR_EOF is not returned if we are reading with our main
+**					prompt is that `builtin_exit` will have been called
+**					from within `input_parse_ctrl_d`, and thus the shell
+**					will have exited.
+**					(See `input_parse_ctrl_d` for more info.)
+**
+**	@return FUNCT_SUCCESS
+**					This will be returned when a byte is succesfully read.
+**
+**	@return FUNCT_ERROR
+**					This will be returned when an error occured during any stage
+**					of parsing the byte (e.g. allocation error).
+*/
+
 static int	input_parse(t_vshdata *data)
 {
 	int		ret;
@@ -103,6 +134,18 @@ static int	input_parse(t_vshdata *data)
 	}
 	return (FUNCT_SUCCESS);
 }
+
+/*
+**	During the readloop, signals will be ignored. This means that pressing
+**	ctrl+c will result in a `\3` char sent to the shell. Likewise, ctrl+d
+**	will result in a '\4' char being sent to the shell. Consequently, you
+**	are not able to send most common signals to the shell while input is being
+**	read.
+**
+**	Input will by read on a byte-by-byte basis. Each time a character is read,
+**	we will parse it in the function above. If the parsed char was an '\n', it
+**	means that input_read is done.
+*/
 
 int			input_read(t_vshdata *data)
 {
