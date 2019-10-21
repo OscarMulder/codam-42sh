43c6fc202568b8d3dfc228801a419de038146511
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-10-21 16:58:35 +0200

    sorted insert ast

--- srcs/globbing/glob_ast_add.c
+++ srcs/globbing/glob_ast_add.c
@@ -6,7 +6,7 @@
 /*   By: mavan-he <mavan-he@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/18 14:38:28 by mavan-he       #+#    #+#                */
-/*   Updated: 2019/10/18 18:11:59 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/10/21 16:51:31 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,40 @@ static t_ast	*ast_new_node(char *value, t_tokens type, char flags)
 	return (node);
 }
 
+static void		glob_insert_ast(t_ast **ast, t_ast *new)
+{
+	t_ast	*probe;
+
+	probe = *ast;
+	if (ft_strcmp(probe->value, new->value) > 0)
+	{
+		new->left = probe;
+		*ast = new;
+		return ;
+	}	
+	while (probe->left != NULL)
+	{
+		if (ft_strcmp(probe->left->value, new->value) > 0)
+		{
+			new->left = probe->left;
+			probe->left = new;
+			return ;
+		}
+		probe = probe->left;
+	}
+	probe->left = new;
+}
+
+/*
+**	glob_ast_add_left always adds new nodes to the left side of our ast
+**	When ast already exists it will insert the new node in a way that the list
+**	remains in alphabetical order
+*/
+
 int				glob_ast_add_left(t_ast **ast, char *value,
 				t_tokens type, char flags)
 {
 	t_ast	*new_node;
-	t_ast	*probe;
 
 	new_node = ast_new_node(value, type, flags);
 	if (new_node == NULL)
@@ -44,11 +73,6 @@ int				glob_ast_add_left(t_ast **ast, char *value,
 	if (*ast == NULL)
 		*ast = new_node;
 	else
-	{
-		probe = *ast;
-		while (probe->left != NULL)
-			probe = probe->left;
-		probe->left = new_node;
-	}
+		glob_insert_ast(ast, new_node);
 	return (FUNCT_SUCCESS);
 }
