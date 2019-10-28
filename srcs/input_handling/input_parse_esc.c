072db2de3492edc39be33c7d62be8a923165860e
Oscar Mulder <omulder@student.codam.nl>
2019-10-28 16:19:28 +0100

    Add comments, fix stupidity

--- srcs/input_handling/input_parse_esc.c
+++ srcs/input_handling/input_parse_esc.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/23 16:48:32 by omulder        #+#    #+#                */
-/*   Updated: 2019/10/28 15:16:55 by omulder       ########   odam.nl         */
+/*   Updated: 2019/10/28 16:19:05 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,28 @@ static void	reset(t_searchhistory *searchhistory)
 	ft_bzero(searchhistory, sizeof(t_searchhistory));
 }
 
+/*
+**  Restores shell to a regular prompt and sets variables appropriately
+*/
+
 void		ctrlr_restore_shell(t_vshdata *data)
 {
-	char	*tmp;
-
 	data->input->searchhistory.active = false;
 	shell_display_prompt(data, REGULAR_PROMPT);
-	tmp = data->line->line;
+	if (data->input->searchhistory.result_str != data->line->line)
+		ft_strdel(&data->line->line);
 	data->line->line = data->input->searchhistory.result_str;
 	data->line->len_max = ft_strlen(data->input->searchhistory.result_str);
 	data->line->len_cur = data->line->len_max;
 	data->line->index = data->line->len_max;
 	input_print_str(data, data->line->line);
-	ft_strdel(&tmp);
 	reset(&data->input->searchhistory);
 }
+
+/*
+**  If ctrl+r is active -> clears the line, and restores the shell with the
+**  found match in line.
+*/
 
 int			input_parse_esc(t_vshdata *data)
 {
