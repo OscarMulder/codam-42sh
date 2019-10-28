bb25515953e07cae62581b259537d2697cb14d2a
Oscar Mulder <omulder@student.codam.nl>
2019-10-28 13:59:52 +0100

    Fix line clearing

--- srcs/input_handling/input_parse_char.c
+++ srcs/input_handling/input_parse_char.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 13:33:54 by rkuijper       #+#    #+#                */
-/*   Updated: 2019/10/28 13:15:43 by omulder       ########   odam.nl         */
+/*   Updated: 2019/10/28 13:59:44 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ void		ctrlr_clear_line(t_vshdata *data)
 	data->prompt->prompt_len = 0;
 	curs_move_n_left(data, data->line->index);
 	data->line->line = tmp;
-	// curs_move_n_left(data, ft_strlen(HIST_SRCH_FIRST));
 	tputs(data->termcaps->tc_clear_lines_str, 1, &ft_tputchar);
 }
 
@@ -31,10 +30,10 @@ int			input_parse_char(t_vshdata *data)
 {
 	if (data->input->searchhistory.active)
 	{
+		ctrlr_clear_line(data);
 		data->line->index = ft_strlen(data->line->line);
 		if (input_parse_char_og(data) == FUNCT_ERROR)
 			return (FUNCT_ERROR);
-		ctrlr_clear_line(data);
 		if (data->input->searchhistory.active == false)
 			return (FUNCT_SUCCESS);
 		if (history_ctrl_r(data, false) == FUNCT_ERROR)
