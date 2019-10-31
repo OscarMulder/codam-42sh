8b407524dc605fd00fec86a647b52018de687c12
Oscar Mulder <32016388+OscarMulder@users.noreply.github.com>
2019-10-31 11:34:25 +0100

    Revert "[WIP] Complete jobs management (#228)"
    This reverts commit d01c8338565f2f0487679e645fdb00dd44ef65a8.

--- srcs/exec/exec_external.c
+++ srcs/exec/exec_external.c
@@ -6,15 +6,43 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/31 10:47:19 by tde-jong       #+#    #+#                */
-/*   Updated: 2019/10/31 09:37:12 by rkuijper      ########   odam.nl         */
+/*   Updated: 2019/10/05 11:40:46 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
-#include <signal.h>
 #include <unistd.h>
-#include <termios.h>
 #include <sys/wait.h>
+#include <termios.h>
+#include <signal.h>
+
+static void		term_flags_init(t_termios *termios_p)
+{
+	if (g_state->shell_type == SHELL_NON_INTERACT)
+		return ;
+	termios_p->c_lflag |= (ECHO | ICANON | ISIG);
+	tcsetattr(STDIN_FILENO, TCSANOW, termios_p);
+}
+
+static void		term_flags_destroy(t_termios *termios_p)
+{
+	if (g_state->shell_type == SHELL_NON_INTERACT)
+		return ;
+	termios_p->c_lflag &= ~(ECHO | ICANON | ISIG);
+	tcsetattr(STDIN_FILENO, TCSANOW, termios_p);
+}
+
+static void		exec_bin_handlewait(pid_t pid)
+{
+	int		status;
+
+	status = 0;
+	waitpid(pid, &status, WUNTRACED);
+	if (WIFEXITED(status))
+		g_state->exit_code = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		g_state->exit_code = EXIT_FATAL + WTERMSIG(status);
+}
 
 /*
 **	Check if `char *binary` is valid.
@@ -23,16 +51,34 @@
 **	Set termios structure back to our special values.
 */
 
-static void		exec_bin(char *binary, t_vshdata *data)
+static void		exec_bin(char *binary, char **args, char **vshenviron,
+t_vshdata *data)
 {
-	t_job	*job;
+	pid_t	pid;
 
-	if (data->jobs->active_job->last_proc->no_cmd == false
-		&& exec_validate_binary(binary) == FUNCT_ERROR)
+	if (exec_validate_binary(binary) == FUNCT_ERROR)
 		return ;
-	job = jobs_last_child(data->jobs->active_job);
-	job->last_proc->binary = binary;
-	job->last_proc->redir_and_assign = data->current_redir_and_assign;
+	if (data->term->termios_p != NULL)
+		term_flags_init(data->term->termios_p);
+	pid = fork();
+	if (pid < 0)
+		return (err_void_exit(E_FORK_STR, EXIT_FAILURE));
+	if (pid > 0)
+	{
+		signal(SIGINT, SIG_IGN);
+		if (data->exec_flags & EXEC_ISPIPED)
+			exec_add_pid_to_pipeseqlist(data, pid);
+	}
+	else
+	{
+		signal(SIGINT, SIG_DFL);
+		execve(binary, args, vshenviron);
+		ft_eprintf(E_FAIL_EXEC_P, binary);
+		exit(EXIT_FAILURE);
+	}
+	if (data->exec_flags & EXEC_WAIT)
+		exec_bin_handlewait(pid);
+	term_flags_destroy(data->term->termios_p);
 }
 
 /*
@@ -45,23 +91,27 @@ static void		exec_bin(char *binary, t_vshdata *data)
 
 void			exec_external(char **args, t_vshdata *data)
 {
+	char	**vshenviron;
 	char	*binary;
 
 	binary = ft_strdup(args[0]);
-	if (binary == NULL)
+	vshenviron = env_lsttoarr(data->envlst);
+	if (binary == NULL || vshenviron == NULL)
 	{
+		ft_strdel(&binary);
+		free(vshenviron);
 		ft_eprintf(E_ALLOC_STR);
 		g_state->exit_code = EXIT_FAILURE;
 		return ;
 	}
-	if (data->jobs->active_job->last_proc->no_cmd == true)
-		exec_bin(binary, data);
-	else if (ft_strchr(args[0], '/') == NULL)
+	if (ft_strchr(args[0], '/') == NULL)
 	{
 		ft_strdel(&binary);
 		if (exec_find_binary(args[0], data, &binary) == FUNCT_SUCCESS)
-			exec_bin(binary, data);
+			exec_bin(binary, args, vshenviron, data);
 	}
 	else
-		exec_bin(binary, data);
+		exec_bin(binary, args, vshenviron, data);
+	free(vshenviron);
+	ft_strdel(&binary);
 }
