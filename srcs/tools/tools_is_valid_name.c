b0bb0d56faeb4a675043e74b4fc2bfd799e05884
Marijn Van heesewijk <mavan-he@student.codam.nl>
2019-11-04 10:03:24 +0100

    merge fix

--- srcs/tools/tools_is_valid_name.c
+++ srcs/tools/tools_is_valid_name.c
@@ -6,14 +6,13 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/06/18 16:31:00 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/11/02 15:19:22 by mavan-he      ########   odam.nl         */
+/*   Updated: 2019/11/04 10:03:13 by mavan-he      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vsh.h"
 
 bool	tools_is_valid_name_char(char c)
-=======
 {
 	return (ft_isalnum(c) == true || ft_strchr("*-_~!@#$%^.,[]+/?", c) != NULL);
 }
