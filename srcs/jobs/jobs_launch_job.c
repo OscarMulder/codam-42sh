d437625c3f93246dcc6632ddcb983808e6763b30
Jorn Brinksma <jornbrinksma@gmail.com>
2019-11-05 16:03:24 +0100

    Fix error in previous commit

--- srcs/jobs/jobs_launch_job.c
+++ srcs/jobs/jobs_launch_job.c
@@ -6,7 +6,7 @@
 /*   By: jbrinksm <jbrinksm@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/05 13:02:41 by jbrinksm       #+#    #+#                */
-/*   Updated: 2019/11/05 15:59:25 by jbrinksm      ########   odam.nl         */
+/*   Updated: 2019/11/05 16:02:28 by jbrinksm      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void		jobs_launch_job(t_job *job)
 		jobs_flush_job(job);
 		return ;
 	}
+	jobs_add_job(g_data, job);
 	if (job->bg == true)
 		jobs_bg_job(job, false);
 	else
